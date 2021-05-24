bd3d8d008d7ae5329644da96c4783779a861302b
Fawzi_Dahamsheh <81533065+fawzij2@users.noreply.github.com>
2021-05-24 18:59:25 +0300

    pulse check 11

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,16 @@
 import React from "react";
-
-const ListItem = (prop)=>{
     const {todoList}= prop;
+const ListItem = (prop)=>{
+    const handleClick = ()=>{
+        return todoList.map((elem,i)=>{
+            console.log(elem.id)
+    }
     return (
         <>
         <div>
             <ol>
                 {todoList.map((elem,i)=>{
-                    return <li key={i} order={i}>{elem}</li>
+                    return <li key={i} order={i} onClick={handleClick}>{elem}</li>
                 })}
             </ol>
         </div>
